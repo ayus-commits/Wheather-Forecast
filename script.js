0c74173e4c495f856f828a05427d9e3bc25aaa9c
Ayush <aaayushmalik283@gmail.com>
2025-12-08 20:00:02 +0530

    Nav Bar created

---
+++ script.js
@@ -0,0 +1,49 @@
+//___________________________________________API_________________________________________________________________
+
+// let baseURL = 'http://api.weatherapi.com/v1'
+// let key = '2cab48709326470f96651918250812'
+// let city = 'Kaithal'
+// let days = 7
+// let search = 'kait'
+
+// // async function getCurrent() {
+// //   try {
+// //     const res = await fetch(`${baseURL}/current.json?key=${key}&q=${city}&aqi=yes`);
+// //     const data = await res.json();
+// //     console.log('current' , data);
+// //   } catch (err) {
+// //     console.error(err);
+// //   }
+// // }
+
+// async function getForecast() {
+// try {
+//     const res = await fetch(`${baseURL}/forecast.json?key=${key}&q=${city}&days=${days}&aqi=yes&alert=yes`);
+//     const data = await res.json();
+//     console.log('forecast',data);
+// } catch (err) {
+//     console.error(err);
+// }
+// }
+
+// async function getSearch() {
+// try {
+//     const res = await fetch(`${baseURL}/search.json?key=${key}&q=${search}`);
+//     const data = await res.json();
+//     console.log('search',data);
+// } catch (err) {
+//     console.error(err);
+// }
+// }
+
+// console.log("Hello console!!");
+// // getCurrent();
+// getForecast();
+// getSearch();
+
+
+
+
+
+
+
